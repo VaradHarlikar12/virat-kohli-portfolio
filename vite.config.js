18ca93c17737f9e61642008005c53372e0d902e3
Varad Harlikar <harlikarvarad@gmail.com>
2025-08-22 12:40:12 +0530

    issue resolve testing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
   server: {
     allowedHosts: ["0ccc1b2d0ca7.ngrok-free.app"],
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        beyond: resolve(__dirname, "./nested/beyond.html"),
+        journey: resolve(__dirname, "./nested/journey.html"),
+        stats: resolve(__dirname, "./nested/stats.html"),
+      },
+    },
   },
 });
